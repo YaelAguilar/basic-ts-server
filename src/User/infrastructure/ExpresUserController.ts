201c0158efdadd45fa04e18957c0cab041258aef
YaelAguilar <213452@ids.upchiapas.edu.mx>
2025-05-24 01:49:52 -0600

    [ADD] Express user controller & services container on shared folder

---
+++ src/User/infrastructure/ExpresUserController.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from "express";
+import { ServicesContainer } from "../../shared/infrastructure/ServicesContainer";
+import { UserNotFoundError } from "../domain/UserNotFoundError";
+
+export class ExpressUserController {
+    async getAll(req: Request, res: Response) {
+        const users = await ServicesContainer.user.getAll.run();
+
+        return res.json(users).status(200);
+    }
+
+    async getOneById(req: Request, res: Response) {
+        try {
+            const user = await ServicesContainer.user.getOneById.run(req.params.id);
+
+            return res.json(user).status(200);
+        } catch (error) {
+            if (error instanceof UserNotFoundError) {
+                return res.status(404).json({ message: error.message });
+            }
+
+            throw error;
+        }
+    }
+
+    async create(req: Request, res: Response) {
+        const { id, name, email, createdAt } = req.body as {
+            id: string;
+            name: string;
+            email: string;
+            createdAt: string;
+        };
+        await ServicesContainer.user.create.run(
+            id, 
+            name, 
+            email, 
+            new Date(createdAt)
+        );
+        
+        return res.status(201).send();
+    }
+
+    async edit(req: Request, res: Response) {
+        const { id, name, email, createdAt } = req.body as {
+            id: string;
+            name: string;
+            email: string;
+            createdAt: string;
+        };
+        await ServicesContainer.user.edit.run(
+            id, 
+            name, 
+            email, 
+            new Date(createdAt)
+        );
+        
+        return res.status(201).send();
+    }
+
+    async delete(req: Request, res: Response) {
+        await ServicesContainer.user.delete.run(req.params.id);
+        
+        return res.status(204).send();
+    }
+}
